124c1b882b3896c00c616e0809a3e1d58c63c0ac
Cloudburst <serek.lego@gmail.com>
2021-08-15 13:53:06 +0200

    oops

--- untested_addons.py
+++ untested_addons.py
@@ -20,7 +20,6 @@ del repo
 
 # function that formats repos contents to exclute private and tested repos and repos which are just unmodified templates and add code size property
 def parse_repo(repo):
-    repo = repo['repository']
     if repo['private']:
         return None
     if repo['html_url'] in tested:
@@ -36,10 +35,11 @@ def parse_repo(repo):
 # Request all code snippets in java extending MeteorAddon class
 r = requests.get(f"https://api.github.com/search/code?q=extends+MeteorAddon+language:java+in:file+fork:true&per_page=100&access_token={GH_TOKEN}")
 repos = r.json()['items']
+repos = [repo['repository'] for repo in repos]
 
 # Request all forks of meteor-addon-template because some people cant click generate
 r = requests.get("https://api.github.com/repos/MeteorDevelopment/meteor-addon-template/forks?per_page=100")
-repos.extend(r.json()['items'])
+repos.extend(r.json())
 
 # load repos
 repos = [parse_repo(repo) for repo in repos]

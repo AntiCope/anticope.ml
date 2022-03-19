901c3ddad5f62ef6742c2c05e24176488ea56493
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-03-19 14:21:26 +0100

    asdf

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders index', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/View Addons/i);
   expect(linkElement).toBeInTheDocument();
 });

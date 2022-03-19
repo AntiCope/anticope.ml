ddef11507297ef21eb1f1b9f28428e41d7f62722
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-03-19 14:18:35 +0100

    sort by search

--- src/pages/addons.js
+++ src/pages/addons.js
@@ -30,12 +30,24 @@ function AddonsPage() {
             })
     }
 
+    function weight(addon) {
+        if (filter.query === "")
+            return addon.verified?1:0;
+        return (
+            compareTwoStrings(filter.query, addon.name.toLowerCase()) 
+            + compareTwoStrings(filter.query, addon.authors.join(" ").toLowerCase())*0.6
+            + compareTwoStrings(filter.query, addon.summary.toLowerCase())*0.4
+            + addon.verified?0.5:0) / 2.5;
+    }
+
     function shouldShow(addon) {
         if (filter.verified && !addon.verified) return false
         if (filter.query !== "") {
             if (compareTwoStrings(filter.query, addon.name.toLowerCase()) < 0.3) {
-                if (compareTwoStrings(filter.query, addon.summary.toLowerCase()) < 0.5) {
-                    return false
+                if (compareTwoStrings(filter.query, addon.authors.join(" ").toLowerCase()) < 0.4) {
+                    if (compareTwoStrings(filter.query, addon.summary.toLowerCase()) < 0.5) {
+                        return false
+                    }
                 }
             }
         }
@@ -51,7 +63,7 @@ function AddonsPage() {
             </Tooltiped>
         </header>
         <section className="addon-grid">
-            {addons.filter(shouldShow).map((addon) => {
+            {addons.filter(shouldShow).sort((a,b) => weight(b) - weight(a)).map((addon) => {
                 return <AddonCard key={addon.id} addon={addon} />
             })}
         </section>

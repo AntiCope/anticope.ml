102ae6de13c18fed1b4e356e9357571142a501fc
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-03-20 11:25:30 +0100

    oops

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,9 +3,9 @@ import {Helmet} from "react-helmet";
 
 function Head({title, summary}) {
     return <Helmet>
-        <title>AntiCope | {title}</title>
-        <meta property="og:title" content={AntiCope | {title}} />
-        <meta property="twitter:title" content={AntiCope | {title}} />
+        <title>{'AntiCope | '}{title}</title>
+        <meta property="og:title" content={"AntiCope | "+title} />
+        <meta property="twitter:title" content={"AntiCope | "+title} />
         <meta name="description" content={summary} />
         <meta name="og:description" content={summary} />
     </Helmet>

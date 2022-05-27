e85afbb67a02e062fc961c67eb02bb2078e3910f
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-05-27 23:25:01 +0200

    use normal 2d canvas instad of shadertoy

--- src/components/End.js
+++ src/components/End.js
@@ -1,80 +1,133 @@
-import React from "react";
 
-import './End.css';
+import React, { useRef, useEffect } from "react";
 
-import ShadertoyReact from 'shadertoy-react';
+import "./End.css"
 
-const shader = `
-#define NOISE_CUTOFF 0.73
-#define SPEED 10.0
+const size = .5;
+const overflow = .7;
+const max_speed = 4;
 
-float rand(vec2 n) { 
-	return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+const particle_types = {
+    0: { color: '77, 82, 93', dir: .2, scale: 2.74, speed: .25 },
+    1: { color: '96, 138, 132', dir: -.4, scale: 5.17, speed: .375 },
+    2: { color: '24, 47, 92', dir: 1.5, scale: 6.54, speed: .5 },
+    3: { color: '33, 73, 71', dir: -0.8, scale: 8, speed: 1 }
 }
 
-float noise(vec2 p){
-	vec2 ip = floor(p);
-	vec2 u = fract(p);
-	u = u*u*(3.0-2.0*u);
-	
-	float res = mix(
-		mix(rand(ip),rand(ip+vec2(1.0,0.0)),u.x),
-		mix(rand(ip+vec2(0.0,1.0)),rand(ip+vec2(1.0,1.0)),u.x),u.y);
-	return res*res;
+function render() {
+    window.context.clearRect(0, 0, window.width, window.height);
+
+    window.particles.forEach((particle) => {
+        const type = particle_types[particle.type]
+
+        window.context.fillStyle = `rgb(${type.color})`;
+        window.context.save()
+        window.context.translate(particle.x, particle.y)
+        window.context.rotate(type.dir * Math.PI);
+        window.context.fillRect(0, 0, type.scale * size, type.scale * size);
+        window.context.fillStyle = `rgba(${type.color}, 0.5)`;
+        window.context.fillRect(-type.scale * size, 0, type.scale * size, type.scale * size);
+        window.context.fillStyle = `rgba(${type.color}, 0.3)`;
+        window.context.fillRect(-type.scale * size * 2, 0, type.scale * size, type.scale * size);
+        window.context.restore()
+
+        particle.x += Math.max(Math.min(Math.cos(type.dir * Math.PI) * 0.7 + window.velocity.tx * type.speed / 8, max_speed), -max_speed) * 0.05;
+        particle.y += Math.max(Math.min(Math.sin(type.dir * Math.PI) * 0.7 + window.velocity.ty * type.speed / 8, max_speed), -max_speed) * 0.05;
+
+        wrap(particle)
+    });
+
+    requestAnimationFrame(render);
 }
 
-float samplePoint(vec2 p) {
-    float n = noise(p);
-    n *= n * n * n;
-    n = step(NOISE_CUTOFF, n);
-    
-    return n;
+function wrap(particle) {
+    if (!particle) return;
+    if (particle.x < -overflow) particle.x = window.width + overflow / 2;
+    if (particle.y < -overflow) particle.y = window.height + overflow / 2;
+    if (particle.x > window.width + overflow) particle.x = -overflow / 2;
+    if (particle.y > window.height + overflow) particle.y = -overflow / 2;
 }
 
-float layer(vec2 fragCoord, float scale) {
-    float n = 0.0;
-    
-    vec2 squared = floor((fragCoord + vec2(0, SPEED*iTime))/scale);
-    n += samplePoint(squared);
-    n += 0.5*samplePoint(squared - vec2(0., 1.));
-    n += 0.25*samplePoint(squared - vec2(0., 2.));
-    n += 0.125*samplePoint(squared - vec2(0., 3.));
+function resize() {
+    window.scale = window.devicePixelRatio || 1;
 
-    return n;
+    window.width = window.innerWidth * window.scale / 4;
+    window.height = window.innerHeight * window.scale / 4;
+
+    const count = Math.sqrt(window.width * window.height) * 3;
+
+    window.canvas.width = window.width;
+    window.canvas.height = window.height;
+
+    window.particles = []
+    for (let i = 0; i < count; i++) {
+        window.particles.push({
+            x: Math.random() * window.width,
+            y: Math.random() * window.height,
+            type: Math.max(Math.floor(Math.random() * 5 - 1), 0)
+        });
+    }
 }
 
-mat2 rotate2d(float _angle) {
-    return mat2(cos(_angle),-sin(_angle),
-                sin(_angle),cos(_angle));
+function movePointer(x, y) {
+    if (typeof window.pointerX === "number" && typeof window.pointerY === "number") {
+        let ox = x - window.pointerX,
+            oy = y - window.pointerY;
+        window.velocity.tx = window.velocity.tx + (ox / 8 * window.scale) * (window.gyroscopeInput ? 1 : -1);
+        window.velocity.ty = window.velocity.ty + (oy / 8 * window.scale) * (window.gyroscopeInput ? 1 : -1);
+    }
+
+    window.pointerX = x;
+    window.pointerY = y;
 }
 
-vec3 decodeSRGB(vec3 screenRGB) {
-    vec3 a = screenRGB / 12.92;
-    vec3 b = pow((screenRGB + 0.055) / 1.055, vec3(2.4));
-    vec3 c = step(vec3(0.04045), screenRGB);
-    return mix(a, b, c);
+function onMouseMove(event) {
+    if (window.gyroscopeInput) return;
+    movePointer(event.clientX, event.clientY);
 }
 
-void main(void) {
-    vec3 c = vec3(0.);
-    vec2 fragCoord = vec2(gl_FragCoord.x, gl_FragCoord.y);
-    vec2 mouse = vec2(iMouse.x, iMouse.y)*0.05;
-    c += 1.4*layer(rotate2d(0.2)*(fragCoord+mouse), 4.0) * vec3(32./255., 57./255., 99./255.);
-    c += 1.73*layer(rotate2d(-0.4)*(fragCoord+mouse), 6.0) * vec3(34./255., 67./255., 75./255.);
-    c += 1.87*layer(rotate2d(1.5)*(fragCoord+mouse), 8.0) * vec3(37./255., 72./255., 72./255.);
-    c += 2.*layer(rotate2d(-0.8)*(fragCoord+mouse), 16.0) * vec3(41./255., 74./255., 61./255.);
+function onMouseLeave() {
+    if (window.gyroscopeInput) return;
+    window.pointerX = null;
+    window.pointerY = null;
+}
 
-    gl_FragColor = vec4(decodeSRGB(c),1.0);
-}`
+function onDeviceOrientation(event) {
+    window.lastGyro = Date.now();
+    window.gyroscopeInput = true;
+    movePointer(event.gamma * 2, event.beta * 2);
+    setTimeout(() => {
+        if (Date.now() - window.lastGyro > 500) window.gyroscopeInput = false;
+    }, 1000);
+}
 
 function End() {
+    const canvasRef = useRef(null)
 
-    return <div className="EndContainer" style={{background:"url('/end.webp')"}}>
-        <ShadertoyReact fs={shader} style={{
-            width: "100vw",
-            height: "100vh"
-        }}/>
-    </div>
+    useEffect(() => {
+        window.context = 1; window.width = 1; window.height = 1; window.scale = 1;
+        window.particles = [];
+        window.pointerX = null; window.pointerY = null;
+
+        window.velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
+        window.gyroscopeInput = false;
+        window.lastGyro = null;
+
+        window.canvas = canvasRef.current;
+        window.canvas.style.backgroundColor = 'rgb(3, 12, 21)';
+        window.context = window.canvas.getContext("2d");
+
+        resize();
+
+        window.addEventListener("resize", resize);
+        window.addEventListener("deviceorientation", onDeviceOrientation);
+        document.body.addEventListener("mousemove", onMouseMove);
+        document.body.addEventListener("mouseleave", onMouseLeave);
+
+        render();
+    }, [canvasRef])
+
+    return <canvas className="EndContainer" ref={canvasRef} />
 }
 
 export default End;

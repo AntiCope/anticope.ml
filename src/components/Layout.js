552fc61c47954b6b805ac254a3436113988794ea
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-06-01 12:24:03 +0200

    use static webp instead of a canvas

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,50 @@
-import React from "react"
+import React, {useEffect, useRef} from "react"
 
 import Header from "./Header"
-import End from "./End"
 import "./Layout.sass"
 
 function Layout({ children }) {
-    return <div className="App">
+    const app = useRef(null)
+
+    useEffect(() => {
+        if (!app || typeof window === "undefined") return;
+
+        if (navigator?.permissions)
+            navigator.permissions.query({name: "accelerometer"})
+
+        var accelX = 0;
+        var accelY = 0;
+
+        window.addEventListener("devicemotion", ev => {
+            if (!app) return;
+
+            accelX = accelX + (ev.acceleration.x*35 - accelX) * 0.3
+            accelY = accelY + (ev.acceleration.y*35 - accelY) * 0.3
+            app?.current?.style?.setProperty(
+                "background-position-x",
+                `${accelX}px`
+            )
+            app?.current?.style?.setProperty(
+                "background-position-y",
+                `${accelY}px`
+            )
+        })
+        window.addEventListener("mousemove", ev => {
+            if (!app) return;
+
+            app?.current?.style?.setProperty(
+                "background-position-x",
+                `${ev.clientX*0.05}px`
+            )
+            app?.current?.style?.setProperty(
+                "background-position-y",
+                `${ev.clientY*0.05}px`
+            )
+        })
+    }, [app])
+
+    return <div className="App" ref={app}>
         <Header />
-        <End />
         <main>
             <div className="Content">
                 {children}

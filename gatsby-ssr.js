39522ec8d0cab4b106cc95529a860d6b57ed3328
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-07-13 15:29:12 +0200

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,6 @@
 const React = require("react")
 
 const HeadComponents = [
- <script
-   key="1-http-ads"
-   data-ad-client="ca-pub-7772000262033925"
-   async
-   crossorigin="anonymous"
-   src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7772000262033925"
-   />,
 ]
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
